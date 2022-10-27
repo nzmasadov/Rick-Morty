de16e37aa16a6d70e8f093f1b9ff6d4bc6de45ee
asadovaa <aynur.asadova@pashabank.az>
2022-10-27 22:07:54 +0400

    Initial Commit

---
+++ ABB_TestCase_nzmasadov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ABB_TestCase_nzmasadov
+//
+//  Created by Test Test on 27.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
